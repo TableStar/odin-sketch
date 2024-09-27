2f5d3baae1b50dc75d598d8e1a818c78df814df2
TableStar <afraroad@gmail.com>
2024-09-27 19:31:54 +0700

    feat: random brush color

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 let size = 16;
+let mode = "black";
 
 const container = document.querySelector(".container");
 const grid = document.createElement("div");
 grid.classList = "grid";
 
-const setGrid = () => {
+const changeButton = document.querySelector(".change");
+const clearButton = document.querySelector(".clear");
+
+const colorCheck = document.querySelector("#color");
+
+const handleSetGrid = () => {
   grid.setAttribute("style", `width:${800 / size}px;height:${800 / size}px`);
   container.innerHTML = "";
   for (let i = 1; i <= size * size; i++) {
@@ -14,14 +20,32 @@ const setGrid = () => {
   const grids = document.querySelectorAll(".grid");
   grids.forEach((gridx) => {
     gridx.addEventListener("mouseover", (e) => {
-      e.target.style["background-color"] = "black";
+      e.target.style["background-color"] = mode;
+      if (mode.includes("rgb")) {
+        mode = rgbRandomizer();
+      }
     });
   });
 };
-setGrid();
 
-const changeButton = document.querySelector(".change");
-const clearButton = document.querySelector(".clear");
+const rgbRandomizer = () => {
+  const r = Math.floor(Math.random()*256)
+  const g = Math.floor(Math.random()*256)
+  const b = Math.floor(Math.random()*256)
+
+  return `rgb(${r}, ${g}, ${b})`
+}
+
+const handleColorCheck = (e) => {
+  if (e.target.checked) {
+    mode = rgbRandomizer();
+  } else {
+    mode = "black";
+  }
+};
+
+handleSetGrid();
+
 changeButton.addEventListener("click", () => {
   let value = prompt("insert number here (max 100)");
   if (!Number.isInteger(value) && !value) {
@@ -33,10 +57,16 @@ changeButton.addEventListener("click", () => {
       size = value;
     }
   }
-  setGrid();
+  handleSetGrid();
+});
+
+colorCheck.addEventListener("change", (e) => {
+  handleColorCheck(e);
 });
 
 clearButton.addEventListener("click", () => {
   size = 16;
-  setGrid();
+  colorCheck.checked = false
+  mode = "black"
+  handleSetGrid();
 });
